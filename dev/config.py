e44e068994214c10310dc5122cbcc5360de9184a
dzmitry_dp <dzmitrupaulenka@outlook.com>
2023-04-15 08:49:07 +0200

    v0.0.62

--- dev/config.py
+++ dev/config.py
@@ -6,13 +6,13 @@ PATH_TO_USER_DB = abspath('./db/user.db')
 # PHONE_NUMBER = '48663215343' # Жека
 PHONE_NUMBER = '48577655470'
 
-from kivy.core.window import Window
+# from kivy.core.window import Window
 
-Window.top = 75
-Window.left = 75
-Window.size = (405, 810)
+# Window.top = 75
+# Window.left = 75
+# Window.size = (405, 810)
 
 from kivy.config import Config
 import os
 
-Config.set('kivy', 'log_dir', os.path.join(os.path.dirname(os.path.abspath(__file__)), os.path.abspath('static/logs')))
+Config.set('kivy', 'log_dir', os.path.join(os.path.dirname(os.path.abspath(__file__)), os.path.abspath('../static/logs')))

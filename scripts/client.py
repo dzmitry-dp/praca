519bde6193466917964b60c43c7802f44ceb860e
dzmitry_dp <dzmitrupaulenka@outlook.com>
2023-04-19 10:50:50 +0200

    v0.0.71

--- scripts/client.py
+++ scripts/client.py
@@ -5,15 +5,19 @@ from threading import Thread
 SERVER = "159.223.25.102"
 PORT = 1489
 
-def listen_server():
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+def forever_listen_server():
     while True:
         try:
+            print('Ожидаю сообщений')
             in_data =  client.recv(4096)
             msg = in_data.decode('utf-8')
 
             if msg != '':
-                print("От сервера :" , msg)
+                print("Принято от сервера :" , msg)
             else:
+                print("Принято от сервера :" , msg)
                 print("Отключение клиента с msg = ''")
                 client.close()
                 break
@@ -22,29 +26,29 @@ def listen_server():
             client.close()
             break
 
-def send_to_server():
-    while True:
-        try:
-            out_data = input()
-            client.sendall(bytes(out_data, 'UTF-8'))
-            print("Отпаравлено :" + str(out_data))
-        except EOFError:
-            print("\nОтключение клиента EOFError")
-            client.close()
-            break
+def send_to_server(data: str):
+    client.sendall(bytes(data, 'UTF-8'))
+    print("Отпаравлено :", data)
 
+def connect_to_server():
+    try:
+        client.connect((SERVER, PORT))
+    except ConnectionRefusedError:
+        print("Подключение не установлено")
+        return False
+    else:
+        # поток для входящей информации
+        input_thread = Thread(target=forever_listen_server)
+        input_thread.start()
+        # input_thread.join()
+        return True
 
 if __name__ == '__main__':
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect((SERVER, PORT))
-
-    # поток для входящей информации
-    input_thread = Thread(target=listen_server)
-    # поток для исходящей информации
-    output_thread = Thread(target=send_to_server)
-
-    input_thread.start()
-    output_thread.start()
-
-    input_thread.join()
-    output_thread.join()
+    print('Start')
+    if connect_to_server():
+        # Поток для исходящей информации
+        output_thread = Thread(target=send_to_server, args=['Pss ... I am here',])
+        output_thread.start()
+        output_thread.join()
+    
+    print('End')
